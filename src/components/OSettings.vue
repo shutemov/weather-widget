43bfd963b347d851c99241a169f858af409bbcf7
Tema <shutemov1997@mail.ru>
2022-09-08 12:45:05 +0300

    feat: creates OSettings

---
+++ src/components/OSettings.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="settings-container">
+    <MWeatherCardHeader
+      title="Settings"
+      actionImg="https://api.iconify.design/heroicons-outline:x.svg"
+      @click:icon="$emit('click:close')"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MWeatherCardHeader from "./MWeatherCardHeader.vue";
+defineEmits(["click:close"]);
+</script>
+
+<style scoped lang="scss"></style>
