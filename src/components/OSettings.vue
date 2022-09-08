9ed919a31cd5ef8fb1ddb3b5035cbbcc4d861bc0
Tema <shutemov1997@mail.ru>
2022-09-08 18:17:50 +0300

    feat: adds setup data for OSettings

--- src/components/OSettings.vue
+++ src/components/OSettings.vue
@@ -10,6 +10,29 @@
 
 <script setup lang="ts">
 import MWeatherCardHeader from "./MWeatherCardHeader.vue";
+
+import { Ref, ref } from "vue";
+import { useDebounceFn } from "@vueuse/core";
+import draggable from "vuedraggable";
+
+import { OPEN_WEATHER_API_KEY } from "@/constants";
+
+type TCity = {
+  name: string;
+  isoCode: string;
+};
+
+type TDataRow = {
+  id?: number;
+  name: string;
+};
+
+const cities: Ref<TDataRow[]> = ref([]);
+const suggestedCities: Ref<TCity[]> = ref([]);
+const cityQuery = ref("");
+
+const drag = ref(false);
+
 defineEmits(["click:close"]);
 </script>
 
