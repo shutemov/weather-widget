4c910293edfae93dc4a664cb89fc4e6a66ee5063
Tema <shutemov1997@mail.ru>
2022-09-08 12:43:50 +0300

    feat: creates MWeatherCardHeader

---
+++ src/components/MWeatherCardHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <h1 class="title">{{ title }}</h1>
+    <div class="gear">
+      <img
+        class="image"
+        alt="Settings"
+        :width="actionImgSize"
+        :height="actionImgSize"
+        :src="actionImg"
+        @click="$emit('click:icon')"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: { type: String, default: "" },
+  actionImgSize: { type: Number, default: 30 },
+  actionImg: {
+    type: String,
+    default: "https://api.iconify.design/typcn:cog-outline.svg",
+  },
+});
+
+defineEmits(["click:settings"]);
+</script>
+
+<style></style>
