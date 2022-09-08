7c29a27b334722d739eed0b8d139e255cd76365e
Tema <shutemov1997@mail.ru>
2022-09-08 12:44:14 +0300

    feat: creates MTemperatureInfo

---
+++ src/components/MTemperatureInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="temperature-info">
+    <img class="image" :alt="`Weather state: ${weatherTitle}`" :src="iconSrc" />
+    <div class="temperature">{{ temperature }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  weatherTitle: { type: String, default: "" },
+  temperature: { type: Number, default: 0 },
+  iconSrc: {
+    type: String,
+    default: "https://api.iconify.design/typcn:cog-outline.svg",
+  },
+});
+
+defineEmits(["click:settings"]);
+</script>
+
+<style></style>
