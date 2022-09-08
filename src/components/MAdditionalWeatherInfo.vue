e9bbdd4ba842335b1f6da9cc0a5161fb40279965
Tema <shutemov1997@mail.ru>
2022-09-08 12:44:31 +0300

    feat: creates MAdditionalWeatherInfo

---
+++ src/components/MAdditionalWeatherInfo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="weather-info">
+    <div class="info-row">
+      <div class="wind-speed">
+        <img
+          class="img"
+          alt="Wind direction"
+          :width="weatherInfoImgSize"
+          :height="weatherInfoImgSize"
+          :src="windDirectionIcon"
+        />
+        <span class="value">{{ windSpeed }} m/s SSE</span>
+      </div>
+      <div class="pressure">
+        <img
+          class="img"
+          :width="weatherInfoImgSize"
+          :height="weatherInfoImgSize"
+          :src="pressureIcon"
+          alt="Barometer"
+        />
+        <span class="value">{{ pressure }} hPa</span>
+      </div>
+    </div>
+    <div class="info-row">
+      <div class="humidity">Humidity: {{ humidity }}%</div>
+      <!-- <div class="dew-point">
+          <span class="value">Dew point: 111 °C</span>
+        </div> -->
+    </div>
+    <div class="info-row">Visibility: {{ visibility }}km</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  weatherInfoImgSize: { type: Number, default: 35 },
+  windSpeed: { type: Number, default: 0 },
+  pressure: { type: Number, default: 0 },
+  humidity: { type: Number, default: 0 },
+  visibility: { type: Number, default: 0 },
+  windDirectionIcon: {
+    type: String,
+    default: "https://api.iconify.design/typcn:location-arrow.svg",
+  },
+  pressureIcon: {
+    type: String,
+    default: "https://api.iconify.design/wi:barometer.svg",
+  },
+});
+</script>
+
+<style></style>
