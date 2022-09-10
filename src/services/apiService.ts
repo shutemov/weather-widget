93f7d4a6d272bdd8765b99971b2af320b72d7db7
Tema <shutemov1997@mail.ru>
2022-09-10 18:24:05 +0300

    feat: adds getCity to api service

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -4,4 +4,7 @@ export default {
   getWeather(query: string): string {
     return `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${OPEN_WEATHER_API_KEY}`;
   },
+  getCity(query: string): string {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${OPEN_WEATHER_API_KEY}`;
+  },
 };
