7fe0413681897af3de36e9bdc31969dbed7813d6
Tema <shutemov1997@mail.ru>
2022-09-10 16:49:27 +0300

    chore: offs multi-word-component-names in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/no-multiple-template-root": "off",
+    "vue/multi-word-component-names": "off",
     "prettier/prettier": [
       "error",
       {
