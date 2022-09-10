f0912cff19bc5ec94fe1e31194ace748fb0b9071
Tema <shutemov1997@mail.ru>
2022-09-10 17:07:26 +0300

    fix: removes App.ce.vue legacy code

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -8,30 +8,7 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { getCurrentInstance, inject, provide } from "vue";
-import { onMounted } from "vue-demi";
-import { set } from "./helpers/storage";
-import { widgetIdKey } from "./symbols";
-import { EStorageKeys } from "./types/storage";
-
-onMounted(async () => {
-  // eslint-disable-next-line no-undef
-  const widgets: NodeListOf<Element> =
-    document.querySelectorAll("weather-widget");
-
-  const arrayIds = [...widgets].map(
-    (item: Element) => (item.childNodes[0] as HTMLElement).innerHTML
-  );
-
-  const data = (widgets[0].childNodes[0] as HTMLElement).innerHTML;
-
-  const widgetId = `${EStorageKeys.WidgetPrefix}-${data}`;
-
-  await set({ key: "ids", value: JSON.stringify(arrayIds) });
-  // await set({ key: widgetId, value: JSON.stringify([]) });
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss">
 #app {
