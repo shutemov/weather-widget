0cf1c4bfb6297b3e93fa929abb28a376ccce44d5
Tema <shutemov1997@mail.ru>
2022-09-10 18:23:18 +0300

    feat: adds getCity request to service

--- src/services/openWeatherService.ts
+++ src/services/openWeatherService.ts
@@ -1,9 +1,15 @@
 import { TOpenWeatherSuccessRequest } from "@/types/openWeather";
+
 import apiService from "@/services/apiService";
+import { TCity } from "@/types/types";
 
 export default {
   async getWeather(query: string): Promise<TOpenWeatherSuccessRequest> {
     const data = await fetch(apiService.getWeather(query));
+    return await data.json();
+  },
+  async getCity(query: string): Promise<TCity[]> {
+    const data = await fetch(apiService.getCity(query));
     return await data.json();
   },
 };
