17a88d664b5422219eee07ff2c8c273c2fedb1fb
Tema <shutemov1997@mail.ru>
2022-09-11 12:11:54 +0300

    feat(router): adds HomeEmpty state

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Settings from "@/components/OSettings.ce.vue";
 import TWeatherInfo from "@/components/TWeatherInfo.ce.vue";
+import TEmptyState from "@/components/TEmptyState.ce.vue";
 
 export enum Route {
   Settings = "settings",
   Home = "home",
+  HomeEmpty = "homeEmpty",
 }
 
 export default createRouter({
@@ -19,6 +21,11 @@ export default createRouter({
       path: "/",
       component: TWeatherInfo,
       name: Route.Home,
+    },
+    {
+      path: "/empty",
+      component: TEmptyState,
+      name: Route.HomeEmpty,
     },
   ],
 });
