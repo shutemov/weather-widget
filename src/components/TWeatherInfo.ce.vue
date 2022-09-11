798df0a55aefc0ec951db901445de4b3abadc766
Tema <shutemov1997@mail.ru>
2022-09-11 12:10:20 +0300

    feat(TWeatherInfo): adds user location temp-re detection

--- src/components/TWeatherInfo.ce.vue
+++ src/components/TWeatherInfo.ce.vue
@@ -12,6 +12,7 @@
       width="30"
       height="30"
       style="position: absolute; top: 0px; right: 8px"
+      class="cog"
       @click="$router.push({ name: Route.Settings })"
     />
     <div
@@ -44,6 +45,8 @@
 
 <script setup lang="ts">
 import { onMounted, Ref, ref } from "vue";
+import { computed, onMounted, Ref, ref } from "vue";
+import { useRouter } from "vue-router";
 
 import OWeatherCard from "@/components/OWeatherCard.ce.vue";
 import OSettings, { TDataRow } from "@/components/OSettings.ce.vue";
@@ -54,18 +57,42 @@ import { EStorageKeys } from "@/types/types";
 import openWeatherService from "@/services/openWeatherService";
 import { TOpenWeatherSuccessRequest } from "@/types/openWeather";
 import { Route } from "@/router";
+import openWeatherService from "@/services/openWeatherService";
 
 const isSettingsOn = ref(false);
 const cities: Ref<TDataRow[]> = ref([]);
 const openWeatherData: Ref<TOpenWeatherSuccessRequest[]> = ref([]);
 
+const router = useRouter();
+
 onMounted(async () => {
   const data = await get(EStorageKeys.Cities);
   cities.value = data !== null ? JSON.parse(data) : [];
 
+  if (!cities.value.length) {
+    navigator.geolocation.getCurrentPosition(
+      async (geoLocationPosition) => {
+        const { latitude, longitude } = geoLocationPosition.coords;
+        const weather = await openWeatherService.getWeatherByLatAndLon(
+          latitude,
+          longitude
+        );
+        openWeatherData.value.push(weather);
+      },
+      (data) => {
+        console.log("error", data);
+        router.push({ name: Route.HomeEmpty });
+      }
+    );
+  }
+
   (async () => {
     for (const city of cities.value) {
       const cityWeather = await openWeatherService.getWeather(city.name);
+    for (const { name } of cities.value) {
+      const cityWeather = await openWeatherService.getWeatherByCityAndCountry(
+        name
+      );
       openWeatherData.value.push(cityWeather);
     }
   })();
@@ -75,5 +102,11 @@ onMounted(async () => {
 <style scoped lang="scss">
 .template-wrapper {
   position: relative;
+
+  > .cog {
+    position: absolute;
+    top: 0px;
+    right: 8px;
+  }
 }
 </style>
