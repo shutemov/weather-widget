dc6598e06fc45bc22576679deb28baae416fd356
Tema <shutemov1997@mail.ru>
2022-09-11 10:52:12 +0300

    fix: removes unused styles

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -7,15 +7,6 @@
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 .main-app {
   min-width: 200px;
   max-width: 350px;
