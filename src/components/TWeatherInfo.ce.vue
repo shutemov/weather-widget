f1102bd687a8ca7d62d84a8d899560dfb2134ee9
Tema <shutemov1997@mail.ru>
2022-09-11 12:11:06 +0300

    fix(TWeatherInfo): cleans code

--- src/components/TWeatherInfo.ce.vue
+++ src/components/TWeatherInfo.ce.vue
@@ -1,40 +1,13 @@
 <template>
   <div class="template-wrapper">
-    <h1
-      v-if="!isSettingsOn && !cities.length"
-      style="font-size: 15pt; width: 80%"
-    >
-      Please select a city in the settings
-    </h1>
     <img
-      v-if="!isSettingsOn"
       src="https://api.iconify.design/typcn:cog-outline.svg"
       width="30"
       height="30"
-      style="position: absolute; top: 0px; right: 8px"
       class="cog"
       @click="$router.push({ name: Route.Settings })"
     />
-    <div
-      v-if="!isSettingsOn && !cities.length"
-      style="
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      "
-    >
-      <img
-        src="https://api.iconify.design/wi:day-haze.svg"
-        width="200"
-        height="200"
-        style=""
-        @click="isSettingsOn = !isSettingsOn"
-      />
-    </div>
     <OWeatherCard
-      v-else
       v-for="(item, index) in openWeatherData"
       :key="index"
       :city-data="item"
@@ -44,17 +17,14 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, Ref, ref } from "vue";
 import { computed, onMounted, Ref, ref } from "vue";
 import { useRouter } from "vue-router";
 
 import OWeatherCard from "@/components/OWeatherCard.ce.vue";
 import OSettings, { TDataRow } from "@/components/OSettings.ce.vue";
-import MWidgetHeader from "@/components/MWidgetHeader.ce.vue";
 
 import { get } from "@/helpers/storage";
 import { EStorageKeys } from "@/types/types";
-import openWeatherService from "@/services/openWeatherService";
 import { TOpenWeatherSuccessRequest } from "@/types/openWeather";
 import { Route } from "@/router";
 import openWeatherService from "@/services/openWeatherService";
@@ -87,8 +57,6 @@ onMounted(async () => {
   }
 
   (async () => {
-    for (const city of cities.value) {
-      const cityWeather = await openWeatherService.getWeather(city.name);
     for (const { name } of cities.value) {
       const cityWeather = await openWeatherService.getWeatherByCityAndCountry(
         name
