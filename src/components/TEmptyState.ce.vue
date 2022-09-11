1836a1ead46216b32194f341f68fb1248b062201
Tema <shutemov1997@mail.ru>
2022-09-11 12:03:08 +0300

    feat(TEmptyState): adds template

--- src/components/TEmptyState.ce.vue
+++ src/components/TEmptyState.ce.vue
@@ -1,5 +1,22 @@
 <template>
+  <div>
+    <MWidgetHeader
+      :title="emptyStateHeader"
+      action-img="https://api.iconify.design/typcn:cog-outline.svg"
+      @click:icon="$router.push({ name: Route.Settings })"
+    />
+    <div class="empty-state-content">
+      <img
+        class="img"
+        src="https://api.iconify.design/wi:day-haze.svg"
+        width="200"
+        height="200"
+      />
+      <h2 class="subtitle">The problem with getting your geolocation</h2>
+    </div>
+  </div>
 </template>
+
 <script lang="ts" setup>
 </script>
 <style scoped lang="scss">
