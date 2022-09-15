d83447a0f43a9174f8f0f2940b0b6d89920eebfe
Tema <shutemov1997@mail.ru>
2022-09-15 20:31:39 +0300

    chore(apiService): moves urls to openWeatherService

--- src/services/openWeatherService.ts
+++ src/services/openWeatherService.ts
@@ -3,13 +3,15 @@ import {
   TOpenWeatherSuccessRequest,
 } from "@/types/openWeather";
 
-import apiService from "@/services/apiService";
+import { OPEN_WEATHER_API_KEY } from "@/constants";
 
 export default {
   async getWeatherByCityAndCountry(
     query: string
   ): Promise<TOpenWeatherSuccessRequest> {
-    const res = await fetch(apiService.getWeatherByCityAndCountry(query));
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
+    );
     const data = await res.json();
 
     if (!res.ok) {
@@ -22,7 +24,9 @@ export default {
     lat: number,
     lon: number
   ): Promise<TOpenWeatherSuccessRequest> {
-    const res = await fetch(apiService.getWeatherByLatAndLon(lat, lon));
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
+    );
     const data = await res.json();
 
     if (!res.ok) {
@@ -32,7 +36,9 @@ export default {
     return data;
   },
   async getCity(query: string): Promise<TOpenWeatherGeoSuccessRequest[]> {
-    const res = await fetch(apiService.getCity(query));
+    const res = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${OPEN_WEATHER_API_KEY}`
+    );
     const data = await res.json();
 
     if (!res.ok) {
